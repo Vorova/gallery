0e98417702f39ba294fee13f1121d1db4dc5f913
vvvorova <vvvorova@gmail.com>
2023-10-09 20:30:26 +0500

    add java doc

--- src/main/java/Gallery.java
+++ src/main/java/Gallery.java
@@ -2,6 +2,12 @@ import java.util.Arrays;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+/**
+ * The dynamic array that contains a regular array under
+ * the hood and modifies it mainly using System.arraycopy()
+ *
+ * @param <T> parametrized type for items
+ */
 public class Gallery<T extends Comparable<T>> {
     private Object[] array;
     private int size = 0;
@@ -14,12 +20,21 @@ public class Gallery<T extends Comparable<T>> {
         array = new Object[capacity];
     }
 
+    /**
+     * This method adds an element to the array
+     * @param el element for adding
+     */
     public void add(T el) {
         if (el == null) throw new NullPointerException();
         if (size == array.length) reduplication();
         array[size++] = el;
     }
 
+    /**
+     * Adds an element to the array with index
+     * @param index index for element
+     * @param el element
+     */
     public void add(int index, T el) {
         if (size == array.length) reduplication();
         if (index < 0 || index > size) throw new ArrayIndexOutOfBoundsException();
@@ -28,17 +43,31 @@ public class Gallery<T extends Comparable<T>> {
         size++;
     }
 
+    /**
+     * Return element by index
+     * @param index for returns elements
+     * @return element
+     */
     public T get(int index) {
         if (index < 0 || index > size) throw new ArrayIndexOutOfBoundsException();
         return (T) array[index];
     }
 
+    /**
+     * Remove element from the list
+     * @param index index for element
+     */
     public void remove(int index) {
         if (index > size || index < 0) throw new ArrayIndexOutOfBoundsException();
         System.arraycopy(array, index + 1, array, index, size - index - 1);
         array[--size] = null;
     }
 
+    /**
+     * Search and remove element from the list if that item exists
+     * @param el element
+     * @return true or false
+     */
     public boolean remove(T el) {
         Optional<Integer> index = getIndex(el);
         if (index.isPresent()) {
@@ -48,23 +77,36 @@ public class Gallery<T extends Comparable<T>> {
         return false;
     }
 
+    /**
+     * @return size for array
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * @return true or false
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Truncates all unused cells
+     */
     public void trim() {
         Object[] newArray = new Object[size];
         System.arraycopy(array, 0, newArray, 0, size);
         array = newArray;
     }
 
+    /**
+     * Sorts this collections
+     */
     public void sort() {
         array = quicksort(this.array);
     }
+
 
     private void reduplication() {
         Object[] x2 = new Object[array.length * 2];
